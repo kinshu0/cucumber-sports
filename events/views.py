1f34425fbe5b2bd5ac4f9f23aeec18a5a721ed3b
Kinshu Gupta <kinshugupta2002@gmail.com>
2020-09-02 12:50:53 -0400

    Got events page completely integrated with proper styling template and base.html file

--- events/views.py
+++ events/views.py
@@ -4,7 +4,7 @@ from .models import Event
 
 # Create your views here.
 def index_view(request):
-    upcoming_events = Event.objects.filter(finished=False).order_by('when')
+    upcoming_events = Event.objects.order_by('when')
 
     return render(request, 'events/events.html', {
         'upcoming_events': upcoming_events,
