b374f3b6e491ad38cc9709dd6ee7d0b90f97d1f1
Kinshu Gupta <kinshugupta2002@gmail.com>
2020-10-12 16:07:39 -0400

    Moving templates directory to root of project in one folder

--- cucumber/settings.py
+++ cucumber/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'cucumber/templates',
+            BASE_DIR / 'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
